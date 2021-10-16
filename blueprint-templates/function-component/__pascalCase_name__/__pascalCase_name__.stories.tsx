973996fbee2e52a8adf528ab98155f2107d56720
Petr Baranov <petr.baranov@bookedby.com>
2021-10-16 12:22:20 +0500

    feat: added storybook, blueprints

---
+++ blueprint-templates/function-component/__pascalCase_name__/__pascalCase_name__.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {{pascalCase name}} from './{{pascalCase name}}';
+
+export default {
+  title: `atoms|{{pascalCase name}}`,
+  component: {{pascalCase name}},
+};
+
+export const {{pascalCase name}}_ = () => {
+  return <{{pascalCase name}} />;
+};
